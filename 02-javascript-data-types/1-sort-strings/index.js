5130cda9253d5ce648566ffea9d0fd4036126ebc
Igor Kuzmin <ikcamowar@mail.ru>
2020-10-04 21:05:38 +0300

    Функция сортировки массива строк

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,20 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const newArr = [...arr];
+  if (param === 'asc') {
+    return newArr.sort(compareStrings);
+  } else {
+    return newArr.sort(compareStrings).reverse();
+  }
+}
 
+function compareStrings(str1, str2) {
+  // если без учета регистра строки совпадают, то возвращаем инвертированный результат сравнения,
+  // чтобы слово в верхнем регистре оказалось первым
+  if (str1.toUpperCase().localeCompare(str2.toUpperCase()) === 0) {
+    return str2.localeCompare(str1);
+  } else {
+    return str1.localeCompare(str2);
+  }
 }
