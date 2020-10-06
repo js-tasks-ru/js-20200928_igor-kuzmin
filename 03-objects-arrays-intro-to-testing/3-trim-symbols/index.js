fd068cdde70cd6ffaa7b079e7a5d1215d882258d
Igor Kuzmin <ikcamowar@mail.ru>
2020-10-07 00:29:53 +0300

    Задачи createGetter, invertObj, trimSymbols, uniq

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (size === 0) {
+    return '';
+  }
+  if (size === undefined) {
+    return string;
+  }
+  let newString = '';
+  let count = 1;
+  let prevChar;
+  for (const char of string) {
+    if (char === prevChar) {
+      count++;
+      if (count <= size) {
+        newString += char;
+      }
+    } else {
+      count = 1;
+      prevChar = char;
+      newString += char;
+    }
+  }
+  return newString;
 }
