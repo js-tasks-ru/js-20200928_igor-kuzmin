564d6a5b7d856e631f56d941121ef1bc98da99b3
Igor Kuzmin <ik@krista.ru>
2020-10-15 15:59:40 +0300

    Решение задачи notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,41 @@
 export default class NotificationMessage {
+  element;
 
+  constructor(inputText = '', {duration = 0, type = 'success'} = {}) {
+    this.inputText = inputText;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  getElement() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration}ms">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.inputText}
+      </div>
+    </div>
+  </div>`;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getElement();
+    this.element = element.firstElementChild;
+  }
+
+  show(elem = document.body) {
+    elem.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
